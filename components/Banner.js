e718a52d1e896dda021759ddf3c0444c214a3fe8
Tony Tony Woodworth <102385518+Anthony-eng21@users.noreply.github.com>
2023-11-16 22:30:09 -0800

    oi ye
    ye

---
+++ components/Banner.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0); //loop state for initializing our work 'animation'
+  const [isDeleting, setIsDeleting] = useState(false); //is the word being deleted or initialized? state
+  const toRate = ["Team Player", "Web Developer", "Mobile Developer", "Entrepeneur"];
+  const [text, setText] = useState(""); // set each letter like w then e then b etc in our arr
+  const [delta, setDelta] = useState(300 - Math.random() * 100); //how fast one letter comes after the first one is typed
+  const period = 2000; //check the duration of each word changing
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRate.length;
+    let fullText = toRate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(900);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome, this is my Portfolio.</span>
+            <h1>
+              {`Hi I'm Tony Woodworth`}
+              <br />
+              <span className="wrap">{text}</span>
+              <p style={{marginTop: 10}}>I specialize in creating full-stack applications for web and mobile platforms.
+              With a passion for clean code and intuitive user experiences, I want to bring your ideas to life. When I'm not coding, you can often find me skateboarding and hiking in my free time.</p>
+            </h1>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ components/Skills.js
@@ -0,0 +1,115 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+
+export const Skills = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Coding Proficiencies</h2>
+              <p>
+                JS, TS, PHP, C++, Node/Express.js, React.js, React Native, SQL, MySQL,
+                MongoDb, Mongoose(ODM), THREE.js +
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skills-slider"
+              >
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>Communication</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="" />
+                  <h5>Mobile Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>Design</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>Frontend</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>Backend</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>OOP</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>JavaScript</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>React.JS</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>React Native</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>Express.JS</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>NoSQL Databases</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>PHP</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>SQL</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>Relational Databases</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>Package Managers</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-left" src={colorSharp} alt="" />
+    </section>
+  );
+};
